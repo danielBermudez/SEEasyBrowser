3285e81dcf50f998e27b6df964df9571132689b9
Daniel Bermúdez <daniel.bermudez@endava.com>
2019-03-12 10:09:27 -0500

    Lesson01 Complete

--- EasyBrowser-lesson01/EasyBrowser/ViewController.swift
+++ EasyBrowser-lesson01/EasyBrowser/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func openPageInSafari(_ sender: UIButton) {
+        // open http://developer.apple.com
+        if let url = URL(string: "http://developer.apple.com"){
+            UIApplication.shared.openURL(url)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
