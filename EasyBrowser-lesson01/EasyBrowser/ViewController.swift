74c76457bbd7984ec199e9bc695f8a5e0bd8e6c2
Daniel Bermúdez <daniel.bermudez@endava.com>
2019-03-13 08:41:31 -0500

    Lesson05 Assignment Complete

--- EasyBrowser-lesson01/EasyBrowser/ViewController.swift
+++ EasyBrowser-lesson01/EasyBrowser/ViewController.swift
@@ -9,24 +9,34 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
-    @IBAction func goBack(_ sender: Any) {
+    @IBAction func goBack(_ sender: UIButton) {
         if webView.canGoBack{
         webView.goBack()
+        } else {
+            sender.isEnabled = false
         }
     }
     
-    @IBAction func goForward(_ sender: Any) {
+    @IBAction func goForward(_ sender: UIButton) {
         if webView.canGoForward{
             webView.goForward()
+        } else {
+            sender.isEnabled = false
         }
     }
     
+    @IBOutlet weak var goForward: UIBarButtonItem!
+    @IBOutlet weak var goBack: UIBarButtonItem!
     @IBOutlet weak var webView: WKWebView!
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
+    
         if let url = URL(string: textField.text!){
+            UserDefaults.standard.set(url, forKey: "savedUrl")
         webView.load(URLRequest(url: url))
+            goForward.isEnabled = true
+            goBack.isEnabled = true
         }
         return true
         
@@ -34,6 +44,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let savedUrl = UserDefaults.standard.url(forKey: "savedUrl"){
+            webView.load(URLRequest(url: savedUrl))
+        }
     }
 
     override func didReceiveMemoryWarning() {
